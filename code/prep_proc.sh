577699eab3d79f947d898f00e4ca9f70e878cb6b
lromang <luis.roangarci@gmail.com>
2016-02-25 13:37:08 -0600

    STDOUT

--- code/prep_proc.sh
+++ code/prep_proc.sh
@@ -21,7 +21,7 @@ ssconvert $file $file
 # -----------------------
 # Metadata display
 # -----------------------
-echo "{\"metadata\":[{\"file_name\":\"$1\"},{\"encoding\":[{\"origin\": \"$encode\"},{\"destiny\": \"utf-8\"}]},{\"size\":[{\"bytes\":\"$(wc -c tmp.csv | sed 's/tmp.csv//g')\"},{\"characters\":\"$(wc -m tmp.csv | sed 's/tmp.csv//g')\"},{\"words\": \"$(wc -w tmp.csv | sed 's/tmp.csv//g')\"},{\"lines\": \"$(wc -l tmp.csv | sed 's/tmp.csv//g')\"},{\"columns\":\"$(cat tmp.csv | awk -F ',' '{print NF}' | head -n 1)\"}]},{\"aditional_info\":[{\"empty_lines\": \"$(cat tmp.csv | grep -E '^$' | wc -l)\"},{\"format_modifications\": \"$(diff correct_file.csv tmp1.csv | wc -l)\"}]}]}" > metadata.txt
+echo "{\"metadata\":[{\"file_name\":\"$1\"},{\"encoding\":[{\"origin\": \"$encode\"},{\"destiny\": \"utf-8\"}]},{\"size\":[{\"bytes\":\"$(wc -c tmp.csv | sed 's/tmp.csv//g')\"},{\"characters\":\"$(wc -m tmp.csv | sed 's/tmp.csv//g')\"},{\"words\": \"$(wc -w tmp.csv | sed 's/tmp.csv//g')\"},{\"lines\": \"$(wc -l tmp.csv | sed 's/tmp.csv//g')\"},{\"columns\":\"$(cat tmp.csv | awk -F ',' '{print NF}' | head -n 1)\"}]},{\"aditional_info\":[{\"empty_lines\": \"$(cat tmp.csv | grep -E '^$' | wc -l)\"},{\"format_modifications\": \"$(diff correct_file.csv tmp1.csv | wc -l)\"}]}]}" # > metadata.txt
 
 
 # -----------------------
