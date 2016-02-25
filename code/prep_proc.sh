42e41e598f3d465cfbb577d5f39c85bcd69f18f3
lromang <luis.roangarci@gmail.com>
2016-02-25 12:25:53 -0600

    pre proc from file

--- code/prep_proc.sh
+++ code/prep_proc.sh
@@ -4,7 +4,8 @@
 # -----------------------
 # Download file
 # -----------------------
-wget  $1 --output-document tmp.csv
+# wget  $1 --output-document tmp.csv
+cat $1 > tmp.csv
 
 # Llevar registro antes y después de modificaciones
 cp tmp.csv tmp1.csv
@@ -20,7 +21,7 @@ ssconvert $file $file
 # -----------------------
 # Metadata display
 # -----------------------
-echo "{\"metadata\":[{\"URL\":\"$1\"},{\"encoding\":[{\"origin\": \"$encode\"},{\"destiny\": \"utf-8\"}]},{\"size\":[{\"bytes\":\"$(wc -c tmp.csv | sed 's/tmp.csv//g')\"},{\"characters\":\"$(wc -m tmp.csv | sed 's/tmp.csv//g')\"},{\"words\": \"$(wc -w tmp.csv | sed 's/tmp.csv//g')\"},{\"lines\": \"$(wc -l tmp.csv | sed 's/tmp.csv//g')\"}]},{\"aditional_info\":[{\"empty_lines\": \"$(cat tmp.csv | grep -E '^$' | wc -l)\"},{\"format_modifications\": \"$(diff correct_file.csv tmp1.csv | wc -l)\"}]}]}" > metadata.txt
+echo "{\"metadata\":[{\"file_name\":\"$1\"},{\"encoding\":[{\"origin\": \"$encode\"},{\"destiny\": \"utf-8\"}]},{\"size\":[{\"bytes\":\"$(wc -c tmp.csv | sed 's/tmp.csv//g')\"},{\"characters\":\"$(wc -m tmp.csv | sed 's/tmp.csv//g')\"},{\"words\": \"$(wc -w tmp.csv | sed 's/tmp.csv//g')\"},{\"lines\": \"$(wc -l tmp.csv | sed 's/tmp.csv//g')\"},{\"columns\":\"$(cat tmp.csv | awk -F ',' '{print NF}' | head -n 1)\"}]},{\"aditional_info\":[{\"empty_lines\": \"$(cat tmp.csv | grep -E '^$' | wc -l)\"},{\"format_modifications\": \"$(diff correct_file.csv tmp1.csv | wc -l)\"}]}]}" > metadata.txt
 
 
 # -----------------------
